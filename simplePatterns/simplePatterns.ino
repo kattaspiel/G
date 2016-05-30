567495ca277e6e0779a3f652c799824d02ac300c
katta <katharina.spiel@tuwien.ac.at>
2016-05-30 18:58:10 +0200

    simple random patterns
    this code produces random patterns with a button press, adding up with
    each push how many lights are used up to 32 of a 64 neopixel matrix.

--- simplePatterns/simplePatterns.ino
+++ simplePatterns/simplePatterns.ino
@@ -5,10 +5,12 @@
 #define LED_PIN 6
 #define BUTTON 2
 
-Adafruit_NeoPixel matrix = Adafruit_NeoPixel(64, PIN, NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel matrix = Adafruit_NeoPixel(64, LED_PIN, NEO_GRB + NEO_KHZ800);
 int buttonState = 0;
 int count = 0;
 uint32_t off = matrix.Color(0 ,0, 0);
+int maximum = 32;
 
 void setup() {
   pinMode(BUTTON, INPUT);
@@ -22,13 +24,31 @@ void loop() {
 
   if(buttonState == HIGH){
     count = count + 1; 
-    if (count == 12){
+    if (count == maximum){
       // turn the matrix off
       count = 0;
-      for (int i; i < matrix.numPixels(); i++){
-          matrix.setPixelColor(i, off); 
+      turn_all_off();
+    }
+    else{
+      // turn on the number of lights acc. to count in a random colour
+      uint32_t colour = get_random_colour();
+      turn_all_off();
+      for(int i = 0; i < count; i++){
+        matrix.setPixelColor(random(64), colour);
       }
       matrix.show();
     }
   }
+  delay(120);
+}
+
+void turn_all_off() {
+  for (int i; i < matrix.numPixels(); i++){
+    matrix.setPixelColor(i, off); 
+  } 
+  matrix.show();  
+}
+
+uint32_t get_random_colour() {
+  return matrix.Color(random(256), random(256), random(256));
 }
